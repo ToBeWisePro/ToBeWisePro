a8d4bc666e30d724612fbc0d3cf24e0be4186eae
Griffin <79300656+griffinclark@users.noreply.github.com>
2023-10-11 15:11:47 -0700

    Added in base analytics

--- src/res/util/RootNavigation.js
+++ src/res/util/RootNavigation.js
@@ -1,40 +1,43 @@
+import React, { useEffect } from "react";
 import { StatusBar } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import {
-  NavigationContainer,
-  type NavigationContainerRef,
-  type ParamListBase,
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
+import * as Notifications from "expo-notifications";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Discover } from "../../interface/Views/Discover";
-import { strings } from "../constants/Strings";
 import { HomeHorizontal } from "../../interface/Views/HomeHorizontal";
 import { EditQuotes } from "../../interface/Views/EditQuote";
 import { HomeVertical } from "../../interface/Views/HomeVertical";
 import { Settings } from "../../interface/Views/Settings";
 import { NotificationScreen } from "../../interface/Views/NotificationsScreen";
 import { NotificationSelectorScreen } from "../../interface/Views/NotificationSelectorScreen";
-import React, { useEffect } from "react";
-import * as Notifications from "expo-notifications";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { strings } from "../constants/Strings";
 import { ASYNC_KEYS } from "../constants/Enums";
+import analytics from "@react-native-firebase/analytics";
 
-interface RootProps {
-  initialRoute: string;
-}
-
-export const navigationRef =
-  React.createRef<NavigationContainerRef<ParamListBase>>();
+export const navigationRef = React.createRef();
 
 const Stack = createStackNavigator();
 
-export const RootNavigation: React.FC<RootProps> = ({
-  initialRoute,
-}: RootProps) => {
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const getActiveRouteName = (navigationState) => {
+  if (navigationState !== null) {
+    return null;
+  }
+  const route = navigationState.routes[navigationState.index];
+  if (route.routes === true) {
+    return getActiveRouteName(route.state);
+  }
+  return route.name;
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type, react/prop-types
+export const RootNavigation = ({ initialRoute }) => {
   useEffect(() => {
     Notifications.addNotificationResponseReceivedListener((response) => {
       void (async () => {
         const quote = response.notification.request.content.data.quote;
-        if (quote === undefined) {
+        if (quote === null) {
           console.error("Data from notification is not defined");
           return;
         }
@@ -67,34 +70,40 @@ export const RootNavigation: React.FC<RootProps> = ({
   }, []);
 
   return (
-    <NavigationContainer ref={navigationRef}>
-      <StatusBar barStyle={"dark-content"} />
+    <NavigationContainer
+      ref={navigationRef}
+      onStateChange={(state) => {
+        const currentRouteName = getActiveRouteName(state);
+        if (currentRouteName !== null) {
+          void analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+      }}
+    >
+      <StatusBar barStyle="dark-content" />
       <Stack.Navigator
         initialRouteName={initialRoute}
         screenOptions={{ animationEnabled: false, headerShown: false }}
       >
         <Stack.Screen name={strings.screenName.discover} component={Discover} />
-        {/* @ts-expect-error Type '({ navigation, route }: Props) => JSX.Element' is not assignable to type 'ScreenComponentType<ParamListBase, string> | undefined'. */}
         <Stack.Screen name={strings.screenName.home} component={HomeVertical} />
         <Stack.Screen
           name={strings.screenName.editQuote}
-          // @ts-expect-error Type '({ navigation, route }: Props) => JSX.Element' is not assignable to type 'ScreenComponentType<ParamListBase, string> | undefined'.
           component={EditQuotes}
         />
         <Stack.Screen
           name={strings.screenName.homeHorizontal}
-          // @ts-expect-error Type '({ navigation, route }: Props) => JSX.Element' is not assignable to type 'ScreenComponentType<ParamListBase, string> | undefined'.
           component={HomeHorizontal}
         />
         <Stack.Screen
           name={strings.screenName.notificationSelectorScreen}
-          // @ts-expect-error Type '({ navigation, route }: Props) => JSX.Element' is not assignable to type 'ScreenComponentType<ParamListBase, string> | undefined'.
           component={NotificationSelectorScreen}
         />
         <Stack.Screen name={strings.screenName.settings} component={Settings} />
         <Stack.Screen
           name={strings.screenName.notificationsScreen}
-          // @ts-expect-error Type '({ navigation, route }: Props) => JSX.Element' is not assignable to type 'ScreenComponentType<ParamListBase, string> | undefined'.
           component={NotificationScreen}
         />
       </Stack.Navigator>
