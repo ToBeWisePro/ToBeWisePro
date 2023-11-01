d64b0df1085e0ca7823cadc84cf00483bf442997
gclark0812@gmail.com <gclark0812@gmail.com>
2023-11-01 15:19:00 -0700

    Made text readable across the quote container

--- src/interface/molecules/QuoteTextAndAuthor.tsx
+++ src/interface/molecules/QuoteTextAndAuthor.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, TouchableOpacity, StyleSheet } from "react-native";
+import { View, TouchableOpacity, ScrollView, StyleSheet } from "react-native";
 import { globalStyles } from "../../../styles/GlobalStyles";
 import { PRIMARY_BLUE } from "../../../styles/Colors";
 import { openLink } from "../../res/functions/UtilFunctions";
@@ -24,28 +24,48 @@ const QuoteTextAndAuthor: React.FC<QuoteTextAndAuthorProps> = ({
   };
 
   return (
-    <View style={styles.quoteAuthorBox}>
-      <AppText style={globalStyles.quoteText} numberOfLines={2048}>
-        {quoteText}
-      </AppText>
-      <TouchableOpacity onPress={handleAuthorLink}>
-        <AppText
-          style={[
-            globalStyles.authorText,
-            { color: PRIMARY_BLUE, paddingTop: 20 },
-          ]}
-        >
-          {author}
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.quoteBox}
+        contentContainerStyle={styles.quoteContentContainer}
+      >
+        <AppText style={[globalStyles.quoteText, styles.quoteText]}>
+          {quoteText}
         </AppText>
-      </TouchableOpacity>
+        <TouchableOpacity onPress={handleAuthorLink} style={styles.authorBox}>
+          <AppText style={[globalStyles.authorText, styles.authorText]}>
+            {author}
+          </AppText>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
-  quoteAuthorBox: {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10,
+  },
+  quoteBox: {
+    flex: 0.8, // Adjust this value to fit the quote in the desired position
+  },
+  quoteContentContainer: {
+    flexGrow: 1,
     justifyContent: "center",
     alignItems: "center",
+    paddingBottom: 0, // Add padding at the bottom
+  },
+  quoteText: {
+    textAlign: "center",
+  },
+  authorBox: {
+    flex: 0.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  authorText: {
+    color: PRIMARY_BLUE,
   },
 });
 
